6a4fbdf9f50011c6028af818a559808d24aa2bed
Syed M Sohaib <syedmsohaib68@gmail.com>
2023-07-24 21:35:18 +0500

    undo commmit

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
-/**
- * Due to Iterative Static Regeneration and the dynamic loading you can get with Vercel in general, 
- * you need to set LD_LIBRARY_PATH properly for all instances of the Vercel processes that are spawned, 
- * not just the initial buildscript.
- * 
- * https://github.com/Automattic/node-canvas/issues/1779
- */
+// /**
+//  * Due to Iterative Static Regeneration and the dynamic loading you can get with Vercel in general, 
+//  * you need to set LD_LIBRARY_PATH properly for all instances of the Vercel processes that are spawned, 
+//  * not just the initial buildscript.
+//  * 
+//  * https://github.com/Automattic/node-canvas/issues/1779
+//  */
 
 
-if (
-    process.env.LD_LIBRARY_PATH == null ||
-    !process.env.LD_LIBRARY_PATH.includes(
-        `${process.env.PWD}/node_modules/canvas/build/Release:`,
-    )
-) {
-    process.env.LD_LIBRARY_PATH = `${
-      process.env.PWD
-    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
+// if (
+//     process.env.LD_LIBRARY_PATH == null ||
+//     !process.env.LD_LIBRARY_PATH.includes(
+//         `${process.env.PWD}/node_modules/canvas/build/Release:`,
+//     )
+// ) {
+//     process.env.LD_LIBRARY_PATH = `${
+//       process.env.PWD
+//     }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+// }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
